7bf42808c844f0e2364f2768f755ca63f6b29b67
haringd <haringd@mail.gvsu.edu>
2018-05-29 14:03:17 -0400

    App is running and calculating, Settings button is not working no settings view added yet.

--- GeoCalculatoriOS/ViewController.swift
+++ GeoCalculatoriOS/ViewController.swift
@@ -7,19 +7,109 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var latitudeP1TextField: UITextField!
+    @IBOutlet weak var longtitudeP1TextField: UITextField!
+    @IBOutlet weak var latitudeP2TextField: UITextField!
+    @IBOutlet weak var longtitudeP2TextField: UITextField!
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var bearingLabel: UILabel!
+    
+    var distanceUnits: String = "Kilometers"
+    var bearingUnits: String = "Degrees"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+/// Dismisses the Keyboard
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        
+        // dismiss keyboard when button pressed
+        self.dismissKeyboard()
+        
+        // used to check for valid points
+        var pointsOk: Bool = false
+        
+        // if optionals not nil
+        if let p1Lat = self.latitudeP1TextField.text, let p1Long = self.longtitudeP1TextField.text, let p2Lat = self.latitudeP2TextField.text, let p2Long = self.longtitudeP2TextField.text {
+            
+            // if text fields not empty strings
+            if p1Lat != "", p1Long != "", p2Lat != "", p2Long != "" {
+                
+                // all points valid
+                pointsOk = true
+            }
+        }
+        
+        // if all points valid
+        if pointsOk {
+            
+            // create CLLocation (points) from latitudes and longitudes
+            let p1: CLLocation = CLLocation(latitude: Double(self.latitudeP1TextField.text!)!, longitude: Double(self.longtitudeP1TextField.text!)!)
+            let p2: CLLocation = CLLocation(latitude: Double(self.latitudeP2TextField.text!)!, longitude: Double(self.longtitudeP2TextField.text!)!)
+            
+            // calculate distance between p1 and p2 in km. round to 2 decimal places
+            var distance: Double = p1.distance(from: p2) / 1000
+            distance = (distance * 100).rounded() / 100
+            
+            // convert distance to miles if distance units is miles
+            if distanceUnits == "Miles" {
+                distance = (distance * 0.621371 * 100).rounded() / 100
+            }
+            
+            // set distance label
+            self.distanceLabel.text = "Distance: \(distance) " + distanceUnits
+            
+            // calculate and set bearing between p1 and p2 in decimal degrees
+            var bearing: Double = p1.bearingToPoint(point: p2)
+            bearing = (bearing * 100).rounded() / 100
+            
+            // convert degrees to mils if bearing units is mils
+            if bearingUnits == "Mils" {
+                bearing = (bearing * 17.777777777778 * 100).rounded() / 100
+            }
+            
+            // set bearing label
+            self.bearingLabel.text = "Bearing: \(bearing) " + bearingUnits
+            
+        } else {
+            self.distanceLabel.text = "Distance:"
+            self.bearingLabel.text = "Bearing:"
+        }
+        
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        // dismiss keyboard when button pressed
+        self.dismissKeyboard()
+        
+        latitudeP1TextField.text = ""
+        latitudeP2TextField.text = ""
+        longtitudeP1TextField.text = ""
+        longtitudeP2TextField.text = ""
+        
+        distanceLabel.text = "Distance:"
+        bearingLabel.text = "Bearing:"
+    }
+    
+    @IBAction func settingsButton(_ sender: UIButton) {
+    }
 }
 
